1ce818eab4cb66fbf284015ca1f6c68c893b74bc
Akhil <akhilu5042@gmail.com>
2022-02-22 15:19:10 +0530

    UC3_EditContact

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -8,8 +8,9 @@ namespace AddressbookSystem
 {
     public class AddressBook
     {
-        ///This list is created to store contacts
+        /// Creates a list to store contact
         List<Contact> contactList;
+        private int zipcode;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AddressBook"/> class.
@@ -22,9 +23,9 @@ namespace AddressbookSystem
         /// Adds the contact  
         public void AddContact()
         {
-            Console.WriteLine("Enter your First Name");
+            Console.WriteLine("Enter First Name");
             string firstName = Console.ReadLine();
-            Console.WriteLine("Enter your Last Name");
+            Console.WriteLine("Enter Last Name");
             string lastName = Console.ReadLine();
             Console.WriteLine("Enter your address");
             string address = Console.ReadLine();
@@ -32,19 +33,142 @@ namespace AddressbookSystem
             string city = Console.ReadLine();
             Console.WriteLine("Enter your state");
             string state = Console.ReadLine();
-            Console.WriteLine("Enter your zip code");
+            Console.WriteLine("Enteryour zip code");
             int zip = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter your phone number");
-            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter your Email-Id");
             string email = Console.ReadLine();
 
-
             ///Creates a reference of Contact class
-            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Contact contact = new Contact(firstName, lastName, address, city, state, zipcode, phoneNumber, email);
             contactList.Add(contact);
             Console.WriteLine(contact);
         }
 
+        /// <summary>
+        /// Displays the menu.
+        /// </summary>
+        public void DisplayMenu()
+        {
+            Console.WriteLine("****MAIN MENU*** : Enter your choice");
+            Console.WriteLine("For Adding contact - please press 1");
+            Console.WriteLine("To Edit contact - please press 2");
+            Console.WriteLine("To Exit - please press 3");
+        }
+
+        /// <summary>
+        /// This method shows the menu for editing the contact
+        /// </summary>
+        public void EditMenu()
+        {
+            Console.WriteLine("Enter 1 to Edit FirstName");
+            Console.WriteLine("Enter 2 to Edit LastName");
+            Console.WriteLine("Enter 3 to Edit Address");
+            Console.WriteLine("Enter 4 to Edit City");
+            Console.WriteLine("Enter 5 to Edit State");
+            Console.WriteLine("Enter 6 to Edit PhoneNumber");
+            Console.WriteLine("Enter 7 to Edit ZipCode");
+            Console.WriteLine("Enter 8 to Edit Email");
+        }
+        /// <summary>
+        /// This method is used to edit the contact
+        /// </summary>
+        public void EditContact()
+        {
+            Console.WriteLine("Enter your first name of the contact you want to edit");
+            string userName = Console.ReadLine();
+
+            for (int index = 0; index < contactList.Count; index++)
+            {
+                if (contactList[index].GetFirstName().Equals(userName))
+                {
+                    EditMenu();
+                    EditContactList(contactList[index]);
+                }
+                else
+                {
+                    Console.WriteLine("First Name does not exist");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Edits the contact list.
+        /// </summary>
+        /// <param name="contact">The contact.</param>
+        public void EditContactList(Contact contact)
+        {
+            Console.WriteLine("Enter your choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first name");
+                    string name = Console.ReadLine();
+                    contact.SetFirstName(name);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the last name");
+                    string lastName = Console.ReadLine();
+                    contact.SetLastName(lastName);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter address");
+                    string address = Console.ReadLine();
+                    contact.SetAddress(address);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter city");
+                    string city = Console.ReadLine();
+                    contact.SetCity(city);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter state");
+                    string state = Console.ReadLine();
+                    contact.SetState(state);
+                    break;
+                case 6:
+                    Console.WriteLine("Enter Phone Number");
+                    string phoneNumber = Console.ReadLine();
+                    contact.SetPhoneNumber(phoneNumber);
+                    break;
+                case 7:
+                    Console.WriteLine("Enter Zip code");
+                    string zipCode = Console.ReadLine();
+                    contact.SetZipCode(zipCode);
+                    break;
+                case 8:
+                    Console.WriteLine("Enter Email");
+                    string email = Console.ReadLine();
+                    contact.SetEmail(email);
+                    break;
+                default:
+                    Console.WriteLine("Enter valid choice");
+                    break;
+            }
+        }
+
+        public void AddressBookMenu()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                DisplayMenu();
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        AddContact();
+                        break;
+                    case 2:
+                        EditContact();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
     }
 }
--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -8,15 +8,16 @@ namespace AddressbookSystem
 {
     public class Contact
     {
+
         /// Variables
-        string firstName;
-        string lastName;
-        string address;
-        string city;
-        string state;
-        int zip;
-        long phoneNumber;
-        string email;
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zipcode;
+        public long phoneNumber;
+        public string email;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Contact"/> class.
@@ -36,9 +37,156 @@ namespace AddressbookSystem
             this.address = address;
             this.city = city;
             this.state = state;
-            this.zip = zip;
+            this.zipcode = zipcode;
             this.phoneNumber = phoneNumber;
             this.email = email;
+        }
+
+        //Get and set methods are used
+        //Whereas get method returns the value of the variable name
+        /// <summary>
+        /// Gets the first name.
+        /// </summary>
+        /// <returns></returns>
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+        /// <summary>
+        /// Gets the last name.
+        /// </summary>
+        /// <returns></returns>
+        public string GetLastName()
+        {
+            return lastName;
+        }
+        /// <summary>
+        /// Gets the address.
+        /// </summary>
+        /// <returns></returns>
+        public string GetAddress()
+        {
+            return address;
+        }
+        /// <summary>
+        /// Gets the city.
+        /// </summary>
+        /// <returns></returns>
+        public string GetCity()
+        {
+            return city;
+        }
+        /// <summary>
+        /// Gets the state.
+        /// </summary>
+        /// <returns></returns>
+        public string GetState()
+        {
+            return state;
+        }
+        /// <summary>
+        /// Gets the email.
+        /// </summary>
+        /// <returns></returns>
+        public string GetEmail()
+        {
+            return email;
+        }
+        /// <summary>
+        /// Gets the phone number.
+        /// </summary>
+        /// <returns></returns>
+        public long GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+        /// <summary>
+        /// Gets the zip.
+        /// </summary>
+        /// <returns></returns>
+        public int GetZip()
+        {
+            return zipcode;
+        }
+        /// <summary>
+        /// Sets the first name.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+
+
+        //Set method takes the parameter that is passed
+        //and variable names are being assigned
+        //Also this keyword is being used to refer the current object
+        public void SetFirstName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+        /// <summary>
+        /// Sets the last name.
+        /// </summary>
+        /// <param name="lastName">The last name.</param>
+        public void SetLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+        /// <summary>
+        /// Sets the address.
+        /// </summary>
+        /// <param name="address">The address.</param>
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+        /// <summary>
+        /// Sets the city.
+        /// </summary>
+        /// <param name="city">The city.</param>
+        public void SetCity(string city)
+        {
+            this.city = city;
+        }
+        /// <summary>
+        /// Sets the state.
+        /// </summary>
+        /// <param name="state">The state.</param>
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+        /// <summary>
+        /// Sets the zip.
+        /// </summary>
+        /// <param name="zip">The zip.</param>
+        public void SetZip(int zip)
+        {
+            this.zipcode = zipcode;
+        }
+        /// <summary>
+        /// Sets the phone number.
+        /// </summary>
+        /// <param name="phoneNumber">The phone number.</param>
+        public void SetPhoneNumber(long phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+
+        internal void SetPhoneNumber(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets the email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        internal void SetZipCode(string zipCode)
+        {
+            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -51,7 +199,7 @@ namespace AddressbookSystem
         public string ToString()
         {
             return "FirstName : " + firstName + " LastName : " + lastName + " Address : " + address + " City : " + city + "State : " + state +
-                " Zip Code :" + zip + " Phone Number : " + phoneNumber + " Email Id : " + email;
+                " Zip Code :" + zipcode + " Phone Number : " + phoneNumber + " Email Id : " + email;
         }
     }
 }
