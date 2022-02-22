549a1142a49294aa6b0d8a7ac8e54ff48bd26bf3
Akhil <akhilu5042@gmail.com>
2022-02-22 14:55:13 +0530

    Welcome to AddressBook Problem statement

---
+++ AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day._06.Addressbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation!");
+        }
+    }
+}
